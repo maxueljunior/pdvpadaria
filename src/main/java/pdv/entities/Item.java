d465c02349a273d64db55f6bd4fd1acfef87f572
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-26 19:31:23 -0300

    Iniciando projeto

---
+++ src/main/java/pdv/entities/Item.java
@@ -0,0 +1,78 @@
+package pdv.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_item")
+public class Item implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Integer id;
+	private String name;
+	private Integer quantidade;
+	private Double preco;
+	
+	public Item() {}
+
+	public Item(Integer id, String name, Integer quantidade, Double preco) {
+		this.id = id;
+		this.name = name;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return Objects.equals(id, other.id);
+	}
+}
