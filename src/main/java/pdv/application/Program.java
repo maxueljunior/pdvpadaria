6680e22520f1e784d0bebbaf827524e956df4caa
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-26 19:39:05 -0300

    Configurando a View

--- src/main/java/pdv/application/Program.java
+++ src/main/java/pdv/application/Program.java
@@ -2,18 +2,25 @@ package pdv.application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Program extends Application {
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage primaryStage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-			 Scene scene = new Scene(parent);
-			 stage.setScene(scene);
-			 stage.show();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/View.fxml"));
+			 ScrollPane scrollPane = loader.load();
+			 
+			 scrollPane.setFitToHeight(true);
+			 scrollPane.setFitToWidth(true);
+			 
+			 Scene mainScene = new Scene(scrollPane);
+			 primaryStage.setScene(mainScene);
+			 primaryStage.setTitle("PDV Padaria");
+			 primaryStage.show();
+			 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
