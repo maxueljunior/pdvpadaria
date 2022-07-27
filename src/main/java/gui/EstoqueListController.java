945ad203c9160839d0603787901cdb7b1cdf0475
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-26 21:20:14 -0300

    Adicionando tela para salvar item no estoque

--- src/main/java/gui/EstoqueListController.java
+++ src/main/java/gui/EstoqueListController.java
@@ -1,17 +1,26 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pdv.application.Program;
 import pdv.model.entities.Item;
@@ -41,8 +50,9 @@ public class EstoqueListController implements Initializable{
 	
 	private ObservableList<Item> obsList;
 	
-	public void onBtnSalvarAction() {
-		System.out.println("salvar...");
+	public void onBtnSalvarAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/ItemForm.fxml", parentStage);
 	}
 	
 	public void setService(ItemService service) {
@@ -69,6 +79,24 @@ public class EstoqueListController implements Initializable{
 		List<Item> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewItem.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Cadastrar Item no Estoque");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(true);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}catch(IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
 	@Override
