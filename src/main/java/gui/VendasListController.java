6c24f9d457d00c899815295e937129b29f8a19a0
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-08-02 21:57:52 -0300

    Finalizando 02/08/22

--- src/main/java/gui/VendasListController.java
+++ src/main/java/gui/VendasListController.java
@@ -1,14 +1,18 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -16,6 +20,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import pdv.application.Program;
 import pdv.model.entities.Item;
 import pdv.model.entities.VendaItem;
 import pdv.model.entities.Vendas;
@@ -37,7 +43,7 @@ public class VendasListController implements Initializable, DataChangeListener {
 	private TableColumn<VendaItem, Integer> tableColumnId;
 	
 	@FXML
-	private TableColumn<VendaItem, String> tableColumnDescri;
+	private TableColumn<Item, String> tableColumnDescri;
 	
 	@FXML
 	private TableColumn<VendaItem, Double> tableColumnPreco;
@@ -77,6 +83,8 @@ public class VendasListController implements Initializable, DataChangeListener {
 
 	@FXML
 	private Button btnPesquisar;
+	
+	private ObservableList<VendaItem> obsList;
 
 	public void setService(VendasService service) {
 		this.service = service;
@@ -127,15 +135,19 @@ public class VendasListController implements Initializable, DataChangeListener {
 			}
 		}
 	}
+	
+	private void initializeNodes() {
+		
+	}
 
 	@Override
 	public void onDataChanged() {
-
+		
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-
+		initializeNodes();
 	}
 
 	public void updateTableView() {
@@ -147,5 +159,6 @@ public class VendasListController implements Initializable, DataChangeListener {
 		}
 		service.saveOrUpdate(vendas);
 		lbVenda.setText("Nº da Venda: " + String.valueOf(vendas.getId()));
+		
 	}
 }
