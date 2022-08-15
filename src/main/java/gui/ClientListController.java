dd051dc2761813af92f0f198bbc665a5621c2e5c
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-08-15 20:17:40 -0300

    Salvando e atualizando cliente

--- src/main/java/gui/ClientListController.java
+++ src/main/java/gui/ClientListController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import pdv.model.entities.Cliente;
 import pdv.model.services.ClienteService;
 
@@ -52,14 +53,34 @@ public class ClientListController implements Initializable{
 	
 	private ObservableList<Cliente> obsList;
 	
+	public void setService(ClienteService service) {
+		this.service = service;
+	}
+	
 	@FXML
 	public void btnSalvarAction() {
-		System.out.println("Salvar");
+		Cliente c = new Cliente();
+
+		c.setName(txtNome.getText());
+		c.setTelefone(txtTelefone.getText());
+		
+		service.saveOrUpdate(c);
+		updateTableView();
+		tableViewCliente.refresh();
 	}
 	
 	@FXML
 	public void btnAtualizarAction() {
-		System.out.println("Atualizar");
+		
+		Cliente c = new Cliente();
+		
+		c.setId(Long.parseLong(txtId.getText()));
+		c.setName(txtNome.getText());
+		c.setTelefone(txtTelefone.getText());
+		
+		service.saveOrUpdate(c);
+		updateTableView();
+		tableViewCliente.refresh();
 	}
 	
 	@FXML
@@ -67,8 +88,17 @@ public class ClientListController implements Initializable{
 		System.out.println("Deletar");
 	}
 	
-	public void setService(ClienteService service) {
-		this.service = service;
+	@FXML
+	public void onClickCliente(MouseEvent e) {
+
+		int i = tableViewCliente.getSelectionModel().getSelectedIndex();
+		
+		Cliente cliente = (Cliente) tableViewCliente.getItems().get(i);
+		
+		txtId.setText(String.valueOf(cliente.getId()));
+		txtNome.setText(cliente.getName());
+		txtTelefone.setText(cliente.getTelefone());
+		
 	}
 	
 	public void updateTableView() {
