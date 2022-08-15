36887ee5386caa2063c5fb120079cb86265c69bf
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-08-15 20:22:58 -0300

    Excluindo e limpando o cache de dados ainda guardados e tirando a seleção da linha da tableview

--- src/main/java/gui/ClientListController.java
+++ src/main/java/gui/ClientListController.java
@@ -17,106 +17,124 @@ import javafx.scene.input.MouseEvent;
 import pdv.model.entities.Cliente;
 import pdv.model.services.ClienteService;
 
-public class ClientListController implements Initializable{
-	
+public class ClientListController implements Initializable {
+
 	private ClienteService service;
-	
+
 	@FXML
 	private TableView<Cliente> tableViewCliente;
-	
+
 	@FXML
 	private TableColumn<Cliente, Long> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Cliente, String> tableColumnNome;
-	
+
 	@FXML
 	private TableColumn<Cliente, String> tableColumnTelefone;
-	
+
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtNome;
-	
+
 	@FXML
 	private TextField txtTelefone;
 
 	@FXML
 	private Button btnSalvar;
-	
+
 	@FXML
 	private Button btnAtualizar;
-	
+
 	@FXML
 	private Button btnDeletar;
-	
+
 	private ObservableList<Cliente> obsList;
-	
+
 	public void setService(ClienteService service) {
 		this.service = service;
 	}
-	
+
 	@FXML
 	public void btnSalvarAction() {
 		Cliente c = new Cliente();
 
 		c.setName(txtNome.getText());
 		c.setTelefone(txtTelefone.getText());
-		
+
 		service.saveOrUpdate(c);
 		updateTableView();
 		tableViewCliente.refresh();
 	}
-	
+
 	@FXML
 	public void btnAtualizarAction() {
-		
+
 		Cliente c = new Cliente();
+
+		c.setId(Long.parseLong(txtId.getText()));
+		c.setName(txtNome.getText());
+		c.setTelefone(txtTelefone.getText());
+
+		service.saveOrUpdate(c);
+		updateTableView();
+		tableViewCliente.refresh();
 		
+		txtId.setText("");
+		txtNome.setText("");
+		txtTelefone.setText("");
+		tableViewCliente.getSelectionModel().clearSelection();
+	}
+
+	@FXML
+	public void btnDeletarAction() {
+		Cliente c = new Cliente();
+
 		c.setId(Long.parseLong(txtId.getText()));
 		c.setName(txtNome.getText());
 		c.setTelefone(txtTelefone.getText());
 		
-		service.saveOrUpdate(c);
+		service.remove(c);
 		updateTableView();
 		tableViewCliente.refresh();
+		
+		txtId.setText("");
+		txtNome.setText("");
+		txtTelefone.setText("");
+		tableViewCliente.getSelectionModel().clearSelection();
 	}
-	
-	@FXML
-	public void btnDeletarAction() {
-		System.out.println("Deletar");
-	}
-	
+
 	@FXML
 	public void onClickCliente(MouseEvent e) {
 
 		int i = tableViewCliente.getSelectionModel().getSelectedIndex();
-		
+
 		Cliente cliente = (Cliente) tableViewCliente.getItems().get(i);
-		
+
 		txtId.setText(String.valueOf(cliente.getId()));
 		txtNome.setText(cliente.getName());
 		txtTelefone.setText(cliente.getTelefone());
-		
+
 	}
-	
+
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Serviço nullo");
 		}
-		
+
 		List<Cliente> lista = service.findAll();
 		obsList = FXCollections.observableArrayList(lista);
 		tableViewCliente.setItems(obsList);
 	}
-	
+
 	public void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("name"));
 		tableColumnTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
