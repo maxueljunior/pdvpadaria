c252bc47adcf4ad762ff6953dc7565100de6a21d
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-08-15 21:19:38 -0300

    Finalizando ClientListController

--- src/main/java/gui/ClientListController.java
+++ src/main/java/gui/ClientListController.java
@@ -2,25 +2,31 @@ package gui;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
+import gui.util.Alerts;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import model.excpetions.ValidationException;
 import pdv.model.entities.Cliente;
 import pdv.model.services.ClienteService;
 
 public class ClientListController implements Initializable {
 
 	private ClienteService service;
-
+	
 	@FXML
 	private TableView<Cliente> tableViewCliente;
 
@@ -51,6 +57,12 @@ public class ClientListController implements Initializable {
 	@FXML
 	private Button btnDeletar;
 
+	@FXML
+	private Label lbNomeError;
+
+	@FXML
+	private Label lbTelefoneError;
+
 	private ObservableList<Cliente> obsList;
 
 	public void setService(ClienteService service) {
@@ -59,33 +71,44 @@ public class ClientListController implements Initializable {
 
 	@FXML
 	public void btnSalvarAction() {
-		Cliente c = new Cliente();
+		try {
+			Cliente c = new Cliente();
+			lbNomeError.setText("");
+			lbTelefoneError.setText("");
+			c = getFormData();
 
-		c.setName(txtNome.getText());
-		c.setTelefone(txtTelefone.getText());
-
-		service.saveOrUpdate(c);
-		updateTableView();
-		tableViewCliente.refresh();
+			service.saveOrUpdate(c);
+			updateTableView();
+			tableViewCliente.refresh();
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		} catch (RuntimeException e) {
+			Alerts.showAlert("Erro ao salvar", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@FXML
 	public void btnAtualizarAction() {
+		try {
+			Cliente c = new Cliente();
+			lbNomeError.setText("");
+			lbTelefoneError.setText("");
+			c = getFormData();
+			
+			c.setId(Long.parseLong(txtId.getText()));
+			service.saveOrUpdate(c);
+			updateTableView();
+			tableViewCliente.refresh();
 
-		Cliente c = new Cliente();
-
-		c.setId(Long.parseLong(txtId.getText()));
-		c.setName(txtNome.getText());
-		c.setTelefone(txtTelefone.getText());
-
-		service.saveOrUpdate(c);
-		updateTableView();
-		tableViewCliente.refresh();
-		
-		txtId.setText("");
-		txtNome.setText("");
-		txtTelefone.setText("");
-		tableViewCliente.getSelectionModel().clearSelection();
+			txtId.setText("");
+			txtNome.setText("");
+			txtTelefone.setText("");
+			tableViewCliente.getSelectionModel().clearSelection();
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		} catch (RuntimeException e) {
+			Alerts.showAlert("Erro ao Atualizar", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@FXML
@@ -95,11 +118,11 @@ public class ClientListController implements Initializable {
 		c.setId(Long.parseLong(txtId.getText()));
 		c.setName(txtNome.getText());
 		c.setTelefone(txtTelefone.getText());
-		
+
 		service.remove(c);
 		updateTableView();
 		tableViewCliente.refresh();
-		
+
 		txtId.setText("");
 		txtNome.setText("");
 		txtTelefone.setText("");
@@ -116,6 +139,9 @@ public class ClientListController implements Initializable {
 		txtId.setText(String.valueOf(cliente.getId()));
 		txtNome.setText(cliente.getName());
 		txtTelefone.setText(cliente.getTelefone());
+		
+		lbNomeError.setText("");
+		lbTelefoneError.setText("");
 
 	}
 
@@ -140,4 +166,36 @@ public class ClientListController implements Initializable {
 		initializeNodes();
 	}
 
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("name")) {
+			lbNomeError.setText(errors.get("name"));
+		}
+		if (fields.contains("telefone")) {
+			lbTelefoneError.setText(errors.get("telefone"));
+		}
+	}
+
+	private Cliente getFormData() {
+
+		Cliente obj = new Cliente();
+
+		ValidationException exception = new ValidationException("Validation error");
+
+		if (txtNome.getText() == "" || txtNome.getText().trim().equals("")) {
+			exception.addError("name", "O Nome está vazio");
+		}
+		obj.setName(txtNome.getText().toUpperCase());
+		if (txtTelefone.getText() == null || txtTelefone.getText().trim().equals("")) {
+			exception.addError("telefone", "O Telefone está vazio");
+		}
+		obj.setTelefone(txtTelefone.getText());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+
+		return obj;
+	}
 }
