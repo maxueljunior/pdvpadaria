32eb7daea0ac4a1dd871bbfddad5880ffd585079
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-09-19 22:01:41 -0300

    abrindo outra venda após finalização de uma

--- src/main/java/gui/VendaFormController.java
+++ src/main/java/gui/VendaFormController.java
@@ -2,8 +2,8 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.Instant;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
@@ -12,18 +12,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import pdv.application.Program;
 import pdv.model.entities.Cliente;
 import pdv.model.entities.Item;
 import pdv.model.entities.Vendas;
 import pdv.model.services.ClienteService;
+import pdv.model.services.VendaItemService;
 import pdv.model.services.VendasService;
 
 public class VendaFormController implements Initializable, DataChangeListener{
@@ -83,8 +90,39 @@ public class VendaFormController implements Initializable, DataChangeListener{
 		
 		Alerts.showAlert("Venda Concluida", null, "A Venda de numero " + numeroVendas + " foi concluida com sucesso!!", AlertType.CONFIRMATION);
 		
+		onMenuItemVendas();
+		
 	}
 	
+	private void onMenuItemVendas() {
+		loadView("/gui/VendasList.fxml", (VendasListController controller) -> {
+			controller.setService(new VendasService());
+			controller.setServiceItem(new VendaItemService());
+			controller.initializeVendas();
+		});
+	}
+	
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initAction) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Program.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbox.getChildren());
+			
+			T controller = loader.getController();
+			initAction.accept(controller);
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregar a pagina", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 	@FXML
 	public void onBtnVendaComEmissaoAction() {
